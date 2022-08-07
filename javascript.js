8e7bd2f623b9aae5816ede2cf788be76d48db34d
Silas Henderson <46079402+SilasHenderson@users.noreply.github.com>
2022-08-07 15:34:12 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -8,8 +8,12 @@ var a = "banana"                   // a is of string type containing "banana" st
 NaN = 5/undefined                  // 'NaN' is a number type
 var thisIsCamelCase = "camel"      // camel-case
 
-/* 'VAR' VS. 'LET' */
+// Arrays JavaScript freecodecamp
 
+.pop(), .push()                    // remove or add to end of array
+.shift, .unshift()                 // remove or add to start of array
+
+/* 'VAR' VS. 'LET' */
 function run(){                    
    {
        var a = 1;                   // 'var' scopes to enclosing function
@@ -27,13 +31,13 @@ function run(){
 /dog|cat/.test('dog')              // true, Alternation OR operator |
 /dog/i.test('Dog')                 // true, ignore case flag i
 /do./.test('dog')                  // true, wildcard character . matches any one character
-/d[uo]g.test('dog')                // true for dug and dog, character class [] defines multiple characters to search for
-/d[u-z]g.test('dog')               // true for dug not dog, search for range in character class with hyphen
+/d[uo]g/.test('dog')               // true for dug and dog, character class [] defines multiple characters to search for
+/d[u-z]g/.test('dog')              // true for dug not dog, search for range in character class with hyphen
 /[^aeiou]/ig.test('dog')           // true, negated character set, matches all characters that are not vowels
 
 "dog and fish".match(/fish/)       // ['fish', index:8, input: 'dog and fish', groups: undefined]
 "dog and dog".match(/dog/g)        // ['dog', 'dog'], global search flag
 "Fish and fish".match(/fish/gi)    // ['Fish', 'fish'], multiple flags
 "dog".match(/[a-z0-9]/)            // ['d', 'o', 'g'], search all letters and numbers within character set
-"3 blind mice.".test(/[^a-z0-9]/ig)// [' ', ' ', '.']
+"3 blind mice.".match(/[^a-z0-9]/ig)// [' ', ' ', '.']
 "aabc".match(/a+/g)                // ['aa'], match character that appears one or more times in a row
